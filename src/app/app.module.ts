3deeb9e21de7d47cd8d3873ff1eb248f2501196f
aniket-fadale <a.fadale@angularminds.in>
2024-01-08 14:08:55 +0530

    dark mode toggle added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonModal, IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent],
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,6 +8,8 @@ register();
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit{
+
+  themeToggle = false;
   sliderConfig = {
     spaceBetween:  1,
     slidesPerView: 1.7
@@ -21,8 +23,35 @@ export class HomePage implements OnInit{
     this.getTodos()
   }
   ngOnInit(): void {
-    
+  // Use matchMedia to check the user preference
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+  // Initialize the dark theme based on the initial
+  // value of the prefers-color-scheme media query
+  this.initializeDarkTheme(prefersDark.matches);
+
+  // Listen for changes to the prefers-color-scheme media query
+  prefersDark.addEventListener('change', (mediaQuery) => this.initializeDarkTheme(mediaQuery.matches));
+}
+
+   // Check/uncheck the toggle and update the theme based on isDark
+   initializeDarkTheme(isDark: boolean) {
+    this.themeToggle = isDark;
+    this.toggleDarkTheme(isDark);
   }
+
+  // Listen for the toggle check/uncheck to toggle the dark theme
+  toggleChange(ev: { detail: { checked: any; }; }) {
+    this.toggleDarkTheme(ev.detail.checked);
+  }
+
+  // Add or remove the "dark" class on the document body
+  toggleDarkTheme(shouldAdd: boolean | undefined) {
+    document.body.classList.toggle('dark', shouldAdd);
+  }
+
+
+
   ionViewWillEnter(){
     this.getUser();
     this.getTodos()
